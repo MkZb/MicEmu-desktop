c1d827537a511267ecb8fe399a9952e5ce47267e
MkZb <MkZb@DESKTOP-TNFHLJ5>
2024-04-16 13:48:41 +0300

    Added socket listening and playback of received packets

--- src/micemu/FXMLController.java
+++ src/micemu/FXMLController.java
@@ -2,19 +2,21 @@ package micemu;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.Line;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.Line.Info;
-import javax.sound.sampled.LineUnavailableException;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ToggleButton;
@@ -30,12 +32,13 @@ public class FXMLController {
 	@FXML
 	private Circle recordingLight;
 
-	private static String fileName = "E:\\test.wav";
-	private final int BUFFER_SIZE = 128000;
-	private AudioInputStream audioStream;
-	private AudioFormat audioFormat;
+	private final int BUFFER_SIZE = 1024;
+	private AudioFormat audioFormat = new AudioFormat(44100, 16, 1, true, false);
 	private SourceDataLine sourceLine;
 	private Mixer mixer;
+
+	int serverPort = 32228;
+	DatagramSocket socket;
 
 	public void initialize() {
 		System.out.println("Controller is working");
@@ -48,54 +51,63 @@ public class FXMLController {
 			if (newValue) {
 				System.out.println("Toggle Button is ON");
 				updateLight(Color.GREEN);
-				playSound();
-
-				// audioFormat = audioStream.getFormat();
-				// DataLine.Info infoIn = new DataLine.Info(SourceDataLine.class, audioFormat);
-
+				openSocket();
 			} else {
 				System.out.println("Toggle Button is OFF");
 				updateLight(Color.RED);
+				closeSocket();
 			}
 		});
 	}
 
-	private void playSound() {
+	private void recievePackets() {
+		Task<Void> reciveAndPlayAudioTask = new Task<Void>() {
+			@Override
+			protected Void call() throws Exception {
+				DataLine.Info infoIn = new DataLine.Info(SourceDataLine.class, audioFormat);
+
+				try {
+					sourceLine = (SourceDataLine) mixer.getLine(infoIn);
+					sourceLine.open(audioFormat);
+				} catch (LineUnavailableException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+
+				sourceLine.start();
+
+				byte[] buffer = new byte[BUFFER_SIZE];
+
+				try {
+					while (true) {
+						DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+						socket.receive(packet);
+						sourceLine.write(packet.getData(), 0, packet.getLength());
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				return null;
+			}
+		};
+
+		new Thread(reciveAndPlayAudioTask).start();
+	}
+
+	private void openSocket() {
 		try {
-			File file = new File(fileName);
-			audioStream = AudioSystem.getAudioInputStream(file);
+			socket = new DatagramSocket(serverPort);
+			recievePackets();
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.exit(1);
 		}
+	}
 
-		audioFormat = audioStream.getFormat();
-		DataLine.Info infoIn = new DataLine.Info(SourceDataLine.class, audioFormat);
-
-		try {
-			sourceLine = (SourceDataLine) mixer.getLine(infoIn);
-			sourceLine.open(audioFormat);
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-			System.exit(1);
+	private void closeSocket() {
+		if (socket != null) {
+			socket.close();
 		}
-
-		sourceLine.start();
-		int nBytesRead = 0;
-		byte[] abData = new byte[BUFFER_SIZE];
-		while (nBytesRead != -1) {
-			try {
-				nBytesRead = audioStream.read(abData, 0, abData.length);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			if (nBytesRead >= 0) {
-				@SuppressWarnings("unused")
-				int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
-			}
-		}
-		sourceLine.drain();
-		sourceLine.close();
 	}
 
 	private void updateLight(Paint paint) {
